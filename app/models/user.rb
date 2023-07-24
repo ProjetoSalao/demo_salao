1a38270424549106002606dab001681c7c2d5ca7
reinaldocosta <reinaldo.costa@gentrop.com>
2023-07-24 18:42:10 -0300

    feat: add tenant to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  acts_as_tenant :tenant
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
+
+  belongs_to :tenant, optional: true
 
   has_one_attached :avatar
   has_person_name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_24_170919) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_212622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -127,8 +127,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_24_170919) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "tenant_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["tenant_id"], name: "index_users_on_tenant_id"
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
@@ -143,4 +145,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_24_170919) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "services", "users"
   add_foreign_key "shops", "tenants"
+  add_foreign_key "users", "tenants"
 end
