e9ad35da4f0c51bfadd0cf2759aaad54a92fdde3
reinaldocosta <reinaldo.costa@gentrop.com>
2023-08-07 17:59:29 -0300

    feat: add user to meeting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_232630) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_184559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_232630) do
     t.datetime "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_meetings_on_user_id"
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -167,6 +169,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_232630) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "meetings", "users"
   add_foreign_key "products", "tenants"
   add_foreign_key "services", "users"
   add_foreign_key "shops", "tenants"
