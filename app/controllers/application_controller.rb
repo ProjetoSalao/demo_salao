8bc93ddc8bc3b8c86f15e09d04be6735f5f97e9a
reinaldocosta <reinaldo.costa@gentrop.com>
2023-09-28 17:35:51 -0300

    chore: workaround for changing tenant_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
-  set_current_tenant_by_subdomain(:tenant, :subdomain)
+  set_current_tenant_through_filter
+  before_action :your_method_that_finds_the_current_tenant
   impersonates :user
   include Pundit::Authorization
 
   protect_from_forgery with: :exception
-
+  
+  # Workaround for login.. it should be on DeviseContoller::Session maybe
+  skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
+  def your_method_that_finds_the_current_tenant
+    return unless current_user.present?    
+    set_current_tenant(current_user.tenant)
+  end  
 
   protected
 
